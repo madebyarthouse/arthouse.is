f3c9279d19692462cfd514589e088b3217a35274
Christian Cito <citochris@gmail.com>
2022-09-11 02:50:36 +0200

    Fix font sizes

--- pages/coming-soon.tsx
+++ pages/coming-soon.tsx
@@ -6,11 +6,11 @@ const Home: NextPage = () => {
   return (
     <>
       <Layout bgClass="brand-bg">
-        <h1 className="font-brand text-6xl md:text-9xl text-center leading-tight">
+        <h1 className="font-brand text-7xl md:text-9xl text-center leading-tight">
           Arthouse
         </h1>
-        <hr className="w-80 border-gray-800 my-5" />
-        <h3 className="text-3xl md:text-4xl text-center leading-tight">
+        <hr className="w-80 max-w-full border-gray-800 my-5" />
+        <h3 className="text-xl md:text-4xl text-center leading-tight">
           A digital product studio <br /> based in Vienna.
         </h3>
       </Layout>
