d1a6e91f493b21f66822123cb66b428b6764c45a
Christian Cito <citochris@gmail.com>
2023-03-23 15:40:00 +0100

    fix entry animations

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -5,11 +5,11 @@ export default function ComingSoonPage() {
       <span className="mix-blend-color-burn bg-green-400/70 rounded-[100px] gate-open-bottom" />
       <span className="mix-blend-color-burn bg-purple-400/70 rounded-[100px] gate-open-right" />
       <span className="mix-blend-color-burn bg-amber-400/70 rounded-[100px] gate-open-left" />
-      <div className="bg-white/90 max-w-[95%] mx-auto relative z-20 p-20 rounded-lg">
-        <span className="shadow-lg mix-blend-darken border-entry-top bg-red-200 shadow-red-200" />
-        <span className=" shadow-lg mix-blend-darken border-entry-bottom bg-purple-200 shadow-purple-200" />
-        <span className="shadow-lg mix-blend-darken border-entry-right bg-green-200 shadow-green-200" />
-        <span className="shadow-lg mix-blend-darken border-entry-left bg-amber-200 shadow-amber-200" />
+      <div className="bg-white/90 max-w-[95%] mx-auto relative z-20 p-5 lg:p-20 rounded-lg">
+        <span className="shadow-lg mix-blend-darken border-entry-top bg-green-200 shadow-green-200" />
+        <span className=" shadow-lg mix-blend-darken border-entry-bottom bg-red-200 shadow-red-200" />
+        <span className="shadow-lg mix-blend-darken border-entry-right bg-amber-200 shadow-amber-200" />
+        <span className="shadow-lg mix-blend-darken border-entry-left bg-purple-200 shadow-purple-200" />
         <h1 className="text-gray-800/80 font-semibold font-brand text-7xl md:text-9xl text-center leading-tight">
           Arthouse
         </h1>
