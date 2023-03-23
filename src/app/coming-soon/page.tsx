f058aff3c20cf494f939fa2e8e1af29f4388fa5a
Christian Cito <citochris@gmail.com>
2023-03-23 17:44:18 +0100

    small responsive style changes

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -42,7 +42,7 @@ export default function ComingSoonPage() {
       <span className="transform-gpu mix-blend-color-burn bg-amber-400/70 rounded-[100px] gate-open-left" />
       <div
         className={clsx(
-          "max-w-[95%] mx-auto relative z-20 p-5 lg:p-20 rounded-lg text-center text-gray-800/80",
+          "max-w-[95%] mx-auto relative z-20 px-5 py-10 md:px-10 lg:p-20 rounded-lg text-center text-gray-800/80",
           showAll && "bg-white/80"
         )}
       >
@@ -82,7 +82,9 @@ export default function ComingSoonPage() {
           }}
           className="flex text-left flex-col items-center gap-1"
         >
-          <span className="font-semibold font-brand text-4xl">We are</span>
+          <span className="font-semibold font-brand text-2xl md:text-4xl">
+            We are
+          </span>
           {/* <AnimatePresence mode="wait"> */}
           <motion.h1
             layout
@@ -90,7 +92,7 @@ export default function ComingSoonPage() {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -25 }}
             key={current}
-            className="font-semibold font-brand text-7xl md:text-9xl text-center leading-tight"
+            className="font-semibold font-brand text-6xl sm:text-7xl md:text-9xl text-center leading-tight"
           >
             {variants[current]}
           </motion.h1>
@@ -115,14 +117,14 @@ export default function ComingSoonPage() {
           >
             <hr className="my-5 w-1/2 mx-auto border-t middle-slide-out border-gray-800/80" />
 
-            <h3 className="!leading-7 text-xl md:text-2xl text-center font-regular font-body">
+            <h3 className="!leading-7 text-xl md:text-3xl text-center font-regular font-body">
               A botique digital product studio <br /> based in Vienna, Austria.
             </h3>
 
             <hr className="my-5 w-1/2 mx-auto border-t middle-slide-out border-gray-800/80" />
 
             <a
-              className="font-semibold active:scale-95 relative text-3xl inline-block group"
+              className="font-semibold active:scale-95 relative text-2xl md:text-3xl inline-block group"
               href="mailto:christian.cito@arthouse.is?subject=Work with us"
             >
               <span>Work with us!</span>
